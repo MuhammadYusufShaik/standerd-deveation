ed3db6dde4b72277ecf9a9aa69f35741cb1112d7
MuhammadYusufShaik <70343761+MuhammadYusufShaik@users.noreply.github.com>
2022-06-25 12:50:29 -0400

    Add files via upload

---
+++ code.py
@@ -0,0 +1,14 @@
+import pandas as pd
+import statistics as st 
+df=pd.read_csv('C:/Users/khada/Downloads/python/c109/height-weight.csv')
+height=df['Height(Inches)'].tolist()
+mean=st.mean(height)
+std=st.stdev(height)
+startingpoint=mean-std
+endingpoint=mean+std
+firstlist=[]
+for h in height:
+    if h > startingpoint and h < endingpoint:
+        firstlist.append(h)
+p=len(firstlist)/len(height)
+print(p*100)        
